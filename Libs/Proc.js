856bf474da5e52dea86fbf551f0e6bb761c26b85
TakamiChie <chie.onpu@gmail.com>
2011-11-24 23:07:12 +0900

    選択範囲にコマンドを適応 　ExecCommand.js追加

--- Libs/Proc.js
+++ Libs/Proc.js
@@ -1,11 +1,25 @@
 var Proc = {
-  execute : function(command){
-    var proc = new ActiveXObject("WScript.Shell").exec(command);
+  escape: true,
+  exec: function(){
+    var s = "";
+    for(var i = 0; i < arguments.length; i++)
+    {
+      if(arguments[i].indexOf(" ") != -1 && escape){
+        s += "\"" + arguments[i] + "\" ";
+      }else{
+        s += arguments[i] + " ";
+      }
+    }
+    return new ActiveXObject("WScript.Shell").exec(s);
+  },
+  execute: function(){
+    var proc = Proc.exec();
     proc.stdIn.Close();
     while(proc.exitCode != 0)
     {
       App.Sleep(10);
     }
     return [proc.stdOut.readAll(), proc.stdErr.readAll()];
-  }
+  },
+  VERSION: 2
 };
