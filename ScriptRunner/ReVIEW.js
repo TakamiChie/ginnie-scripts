a51326e77c9bc8c9556b6ac69196489f1711cf5b
TakamiChie <chie@onpu-tamago.net>
2013-08-24 00:55:33 +0900

    Configファイルが同一ディレクトリに存在した場合は、それを見る処理を追加(config,ymlのファイル名は固定です)

--- ScriptRunner/ReVIEW.js
+++ ScriptRunner/ReVIEW.js
@@ -1,16 +1,25 @@
+// 定数
 var OUTFILE = "output.html";
+var CONFIG  = "config.yml";
+
+// 
 var ScriptRunner = {
   Extension: "re",
   PreProcess: function(file){
     var shell = new ActiveXObject("WScript.Shell");
     var fso = new ActiveXObject("Scripting.FileSystemObject");
-    App.output.clear();
     var nowdir = shell.currentDirectory;
     try{
       // コンパイル処理
       shell.currentDirectory = fso.getParentFolderName(file);
+      // yamlファイルはあるか？
+      var yaml = "";
+      if(fso.fileExists(fso.buildPath(shell.currentDirectory, CONFIG))){
+        yaml = "--yaml=" + CONFIG;
+      }
+      // 実行
       var ret = 
-        Proc.execute("cmd", "/C", "review-compile", "--target",  "html", fso.getFileName(file), "--output-file=" + OUTFILE); 
+        Proc.execute("cmd", "/C", "review-compile", "--target",  "html", fso.getFileName(file), "--output-file=" + OUTFILE, yaml); 
       var outfile = fso.buildpath(shell.currentDirectory, OUTFILE);
 
       if(ret[1] != "")
@@ -21,6 +30,7 @@ var ScriptRunner = {
         var shellapp = new ActiveXObject("Shell.Application");
         var shellwin = shellapp.Windows();
         var ie;
+        // http://takuya-1st.hatenablog.jp/entry/20071112/1194887535
         for(var i = 0; i < shellwin.count; i++){
           var item = shellwin.item(i);
           if(item && item.fullName.match(/iexplore\.exe$/i)) {
