396936cc1be97725ace87636aca803799f92e34f
高見知英 <chie@onpu-tamago.net>
2014-06-19 17:03:46 +0900

    ReView記法のAddonRunnerスクリプトを作成

---
+++ AddonRunner/ReVIEW.js
@@ -0,0 +1,22 @@
+var Items = [
+  {
+    caption: "画像",
+    body: "//image[%%画像IDは？%%][%%キャプションは？%%]"
+  },
+  {
+    caption: "引用",
+    body: "//quote{\n||\n//}"
+  },
+  {
+    caption: "コマンドライン",
+    body: "//cmd{\n||\n//}"
+  },
+  {
+    caption: "テーブル",
+    body: "//table[%%識別子は？%%][%%キャプションは？%%]{\n||\n\n//}"
+  },
+  {
+    caption: "注釈",
+    body: "@<fn>{%%識別子は？:fn%%}\n//footnote[%%fn%%][%%解説文は？%%]\n"
+  }
+]
