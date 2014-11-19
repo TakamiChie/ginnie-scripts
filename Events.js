f2ad9d7487c263cf95cd4de92824fcdb20a68bcc
高見知英 <chie@onpu-tamago.net>
2014-11-19 10:32:36 +0900

    Javascript編集中のコメントインデントに対応

--- Events.js
+++ Events.js
@@ -41,6 +41,30 @@ function onDocumentKeyPress( tabIndex, key)
             }
             break;
       }
+    case "JScript":
+      var l = doc.caret.lineString(doc.caret.line);
+      switch(key){
+        case VK_BACK:
+            // 箇条書きを削除(シフトキー同時押しの時は削除しない)
+            if(!App.keyState(VK_SHIFT)){
+              var m = l.match(/^\s*\/\/\s*$/);
+              if(m != null){
+                  doc.caret.selStart = doc.caret.selStart - m[0].length;
+                  doc.caret.selLength = m[0].length;
+                  doc.caret.deleteSelected();
+                  return 0;
+              }
+            }
+            break;
+        case VK_ENTER:
+            // コメント中に改行したら、次の行もコメントに
+            var m = l.match(/^\s*\/\/\s*/);
+            if(m != null){
+              doc.caret.selText = "\n" + m[0];
+              return 0;
+            }
+            break;
+      }
     break;
   }
   return key;
